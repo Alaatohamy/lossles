a1a3aaeab2ccfb6c7e4b5ce3a6e963260d0a1a64
alaa <alaatohamy4@gmail.com>
2018-08-03 06:56:57 +0200

    Add popup

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,18 @@
-import $ from 'jquery';
+var yes = document.getElementById('yes');
+var no = document.getElementById('no');
 
-$('#yes').on('click', (){
+yes.onclick = function() {
   modal.style.display = "none";
-});
+}
+
+no.onclick = function() {
+  modal.style.display = "none";
+}
+
+var timer = document.getElementById('timer');
+
+timer.onclick = function() {
+  setTimeout(myFunction, 3000){
+    alert('Hello');
+  }
+}
